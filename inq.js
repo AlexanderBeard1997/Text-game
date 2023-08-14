88e924a58b73a0f99a3a76bd42737da9dd0dcf63
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-08-14 16:34:54 +0100

    day 1

--- inq.js
+++ inq.js
@@ -1,10 +1,17 @@
-import inquirer from "inquirer"
+console.clear();
+
+import inquirer from "inquirer";
+// import Kitchen from "./rooms.js";
+// import Breakroom from "./rooms.js";
+//module.imports = {
+//   rooms.js
+//}
 
 export const getName = async () => {
     let {userName} = await inquirer.prompt({
      name: `userName`,
      type: `input`,
-     message: `What is your name?`
+     message: `What is your name?`,
     })
     return userName
  }
@@ -18,12 +25,42 @@ export const getName = async () => {
     return ship
  }
 
+ export const getKitchen = async () =>{ 
+   let {Kitchen} = await inquirer.prompt ({
+   name: `Kitchen`,
+   type: `input`,
+   message: `the kitchen is a mess dishes have shattered and the broken shards are strewn about all ocer the floor. \n
+   The rations have been spilled, resulting in large puddles of liquid and mounds of food. \n
+   Smoke is billowing out from underneath the door to your left, and the one in front of you \n
+   There is a delicious cake on the counter. It is a chocolate gateau with chocolate curls and a strawberry on top of it.\n`,
+  
+   })
+  }
+  
+  export const getBreakroom = async () => {
+   let {Breakroom} = await inquirer.prompt ({
+      name: `Breakroom`,
+      type: `input`,
+      message: `hello`
+   })
+  }
+
  export const selectType = async () => {
-   let {choice} = await inquirer.prompt({
+   let userChoice = await inquirer.prompt({
    name: `choice`,   
    type: `list`,
-   message: `you can either go left or go right`,
-   choices : [ `go left` , `go right `],
+   message: `you can either enter the Breakroom or the Kitchen."`,
+   choices : [ `Breakroom` , `Kitchen `],
    })
-   return choice
-}
+
+   
+ }
+ 
+//   .then((answers)=>{
+//      if(answers.selectType === "Breakroom"){
+//         console.log(`vfgsdga`)
+//        } else if(answers.selectType === "Kitchen"){
+//          console.log(`hgfhdgf`)
+//        }
+//       })
+
